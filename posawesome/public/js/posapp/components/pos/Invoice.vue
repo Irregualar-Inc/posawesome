a53ade745396201e31abf86785a00492b952633e
Olatunji Komolafe <iamkomolafe.o.s@gmail.com>
2024-05-23 13:06:58 +0100

    feat: made code input hidden

--- posawesome/public/js/posapp/components/pos/Invoice.vue
+++ posawesome/public/js/posapp/components/pos/Invoice.vue
@@ -19,23 +19,23 @@
       </v-card>
     </v-dialog>
     <v-dialog v-model="codeDialog" max-width="330" @click:outside="clearCode">
-    <v-card>
-      <v-card-title class="text-h5">
-        <span class="headline primary--text">{{ __("Enter 6-Digit Code") }}</span>
-      </v-card-title>
-      <v-card-text>
-        <v-text-field v-model="code" label="Code" type="text" maxlength="6"></v-text-field>
-        <div v-if="error" class="error-message">
-          <v-icon color="red">mdi-alert-octagon</v-icon> {{ error }}
-        </div>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" @click="validateCode">{{ __("Submit") }}</v-btn>
-        <v-btn color="warning" @click="clearCode">{{ __("Clear") }}</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+      <v-card>
+        <v-card-title class="text-h5">
+          <span class="headline primary--text">{{ __("Enter 6-Digit Code") }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="code" label="Code" type="password" maxlength="6"></v-text-field>
+          <div v-if="error" class="error-message">
+            <v-icon color="red">mdi-alert-octagon</v-icon> {{ error }}
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="validateCode">{{ __("Submit") }}</v-btn>
+          <v-btn color="warning" @click="clearCode">{{ __("Clear") }}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-card
       style="max-height: 70vh; height: 70vh"
       class="cards my-0 py-0 mt-3 grey lighten-5"
